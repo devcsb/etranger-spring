99efe00a39717015e6f6ab1d7e45a4da0fb7a857
devcsb <devcsb119@gmail.com>
2022-06-24 22:37:54 +0900

    build.gradle, application.yml 설정 및 테스트

--- src/main/java/com/csb/etranger/EtrangerApplication.java
+++ src/main/java/com/csb/etranger/EtrangerApplication.java
@@ -3,6 +3,8 @@ package com.csb.etranger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.swing.*;
+
 @SpringBootApplication
 public class EtrangerApplication {
 
