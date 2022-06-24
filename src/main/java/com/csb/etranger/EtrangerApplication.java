8470a4cc9e46d120058bae611ab22f298c71bdbc
devcsb <devcsb119@gmail.com>
2022-06-24 21:18:06 +0900

    initial commit

---
+++ src/main/java/com/csb/etranger/EtrangerApplication.java
@@ -0,0 +1,13 @@
+package com.csb.etranger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtrangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtrangerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csb/etranger/EtrangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csb.etranger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtrangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
